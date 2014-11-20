cfe4be20d4dd938c331b554b667175e94374b81f
progre <djyayutto@gmail.com>
2014-11-20 23:40:03 +0900

    referactoring

--- src/hayo/infrastructure/database.ts
+++ src/hayo/infrastructure/database.ts
@@ -1,14 +1,6 @@
 import Sequelize = require('sequelize');
 import rps = require('../domain/repos/reposes');
-
-var SERIAL = {
-    type: Sequelize.BIGINT,
-    primaryKey: true,
-    autoIncrement: true,
-    get: function () {
-        return this.getDataValue('id').toString(10);
-    }
-};
+import Schema = require('./schema');
 
 class Database {
     static new(dataDir: string) {
@@ -21,57 +13,17 @@ class Database {
             .then(() => database);
     }
 
-    private ticket = this.sequelize.define('Ticket',
-        {
-            id: SERIAL,
-            title: {
-                type: Sequelize.STRING,
-                validate: { len: [1, 32] }
-            },
-            type: Sequelize.INTEGER,
-            url: {
-                type: Sequelize.STRING,
-                validate: { len: [1, 2083] }
-            },
-            openUserId: Sequelize.STRING,
-            progressUserId: Sequelize.STRING,
-            progressAt: Sequelize.DATE,
-            completedAt: Sequelize.DATE
-        }, {
-            updatedAt: false,
-            paranoid: true
-        });
-
-    private likeOpen = this.sequelize.define('LikeOpen',
-        {
-            id: SERIAL,
-            userId: Sequelize.STRING
-        }, {
-            timestamps: false
-        });
-
-    private likeInprogress = this.sequelize.define('LikeInprogress',
-        {
-            id: SERIAL,
-            userId: Sequelize.STRING,
-            count: Sequelize.INTEGER
-        }, {
-            timestamps: false
-        });
+    private schema = new Schema(this.sequelize);
 
     constructor(private sequelize: Sequelize) {
-        this.ticket.hasMany(this.likeOpen);
-        this.likeOpen.belongsTo(this.ticket);
-        this.ticket.hasMany(this.likeInprogress);
-        this.likeInprogress.belongsTo(this.ticket);
     }
 
     tickets(type: rps.TicketType) {
-        return this.ticket
+        return this.schema.ticket
             .findAll({
                 where: { type: type },
                 order: [['ticket.id', 'DESC']],
-                include: [this.likeOpen, this.likeInprogress]
+                include: [this.schema.likeOpen, this.schema.likeInprogress]
             }).then(instances => instances.map(x => ({
                 id: x['id'],
                 createdAt: x['createdAt'],
@@ -85,7 +37,7 @@ class Database {
     }
 
     putTicket(user: rps.User, title: string) {
-        return this.ticket.create({
+        return this.schema.ticket.create({
             title: title,
             type: rps.TicketType.open,
             openUserId: toUserId(user)
@@ -93,135 +45,134 @@ class Database {
     }
 
     deleteTicket(user: rps.User, ticketId: string) {
-        return this.transaction()
-            .then(t => {
-                return this.ticket.findOne(
-                    { where: { id: ticketId } },
-                    { transaction: t })
-                    .then(ticket => {
-                        return ticket.destroy({ transaction: t });
-                    })
-                    .then(() => {
-                        t.commit();
-                    });
-            });
+        return this.transaction().then(t => {
+            return this.schema.ticket.findOne(
+                { where: { id: ticketId } },
+                { transaction: t })
+                .then(ticket => {
+                    return ticket.destroy({ transaction: t });
+                })
+                .then(() => t.commit());
+        });
     }
 
     likeOpenTicket(user: rps.User, ticketId: string) {
-        return this.transaction().then(t => this.ticket.findOne(
-            {
-                where: { id: ticketId },
-                include: [this.likeOpen]
-            },
-            { transaction: t })
-            .then(ticket => ticket['getLikeOpens'](
-                { where: { userId: toUserId(user) } },
+        return this.transaction().then(t => {
+            return this.schema.ticket.findOne(
+                {
+                    where: { id: ticketId },
+                    include: [this.schema.likeOpen]
+                },
                 { transaction: t })
-                .then((likeOpens: SequelizeJS.Instance[]) => {
-                    if (likeOpens.length > 0)
-                        return;
-                    return ticket['createLikeOpen'](
-                        { userId: toUserId(user) },
-                        { transaction: t });
-                }))
-            .then(() => t.commit()));
+                .then(ticket => {
+                    return ticket['getLikeOpens'](
+                        { where: { userId: toUserId(user) } },
+                        { transaction: t })
+                        .then((likeOpens: SequelizeJS.Instance[]) => {
+                            if (likeOpens.length > 0)
+                                return;
+                            return ticket['createLikeOpen'](
+                                { userId: toUserId(user) },
+                                { transaction: t });
+                        });
+                })
+                .then(() => t.commit());
+        });
     }
 
     progressTicket(user: rps.User, ticketId: string) {
-        return this.transaction()
-            .then(t => {
-                return this.ticket.findOne(
-                    { where: { id: ticketId } },
-                    { transaction: t })
-                    .then(ticket => {
-                        ticket['type'] = rps.TicketType.inprogress;
-                        ticket['progressUserId'] = toUserId(user);
-                        ticket['progressAt'] = new Date();
-                        return ticket.save({ transaction: t });
-                    })
-                    .then(() => {
-                        t.commit();
-                    });
+        return this.updateTicket(
+            {
+                id: ticketId
+            },
+            {
+                type: rps.TicketType.inprogress,
+                progressUserId: toUserId(user),
+                progressAt: new Date()
             });
     }
 
     likeInprogressTicket(user: rps.User, ticketId: string) {
-        return this.transaction().then(t => this.ticket.findOne(
-            {
-                where: { id: ticketId },
-                include: [this.likeInprogress]
-            },
-            { transaction: t })
-            .then(ticket => ticket['getLikeInprogresses'](
-                { where: { userId: toUserId(user) } },
+        return this.transaction().then(t => {
+            return this.schema.ticket.findOne(
+                {
+                    where: { id: ticketId },
+                    include: [this.schema.likeInprogress]
+                },
                 { transaction: t })
-                .then((likeInprogresses: SequelizeJS.Instance[]) => {
-                    if (likeInprogresses.length > 0) {
-                        var likeInprogress = likeInprogresses[0];
-                        likeInprogress['count'] += 1;
-                        return likeInprogress.save({ transaction: t });
-                    }
-                    return ticket['createLikeInprogress'](
-                        {
-                            userId: toUserId(user),
-                            count: 1
-                        },
-                        { transaction: t });
-                }))
-            .then(() => t.commit()));
+                .then(ticket => {
+                    return ticket['getLikeInprogresses'](
+                        { where: { userId: toUserId(user) } },
+                        { transaction: t })
+                        .then((likeInprogresses: SequelizeJS.Instance[]) => {
+                            if (likeInprogresses.length > 0) {
+                                var likeInprogress = likeInprogresses[0];
+                                likeInprogress['count'] += 1;
+                                return likeInprogress.save({ transaction: t });
+                            }
+                            return ticket['createLikeInprogress'](
+                                {
+                                    userId: toUserId(user),
+                                    count: 1
+                                },
+                                { transaction: t });
+                        });
+                })
+                .then(() => t.commit());
+        });
     }
 
     reverseTicket(user: rps.User, ticketId: string) {
-        return this.transaction()
-            .then(t => {
-                return this.ticket.findOne(
-                    { where: { id: ticketId, progressUserId: toUserId(user) } },
-                    { transaction: t })
-                    .then(ticket => {
-                        ticket['type'] = rps.TicketType.open;
-                        ticket['progressUserId'] = null;
-                        ticket['progressAt'] = null;
-                        return ticket.save({ transaction: t });
-                    })
-                    .then(() => {
-                        t.commit();
-                    });
+        return this.updateTicket(
+            {
+                id: ticketId,
+                progressUserId: toUserId(user)
+            },
+            {
+                type: rps.TicketType.open,
+                progressUserId: null,
+                progressAt: null
             });
     }
 
     completeTicket(user: rps.User, ticketId: string, url: string) {
-        return this.transaction()
-            .then(t => {
-                return this.ticket.findOne(
-                    { where: { id: ticketId, progressUserId: toUserId(user) } },
-                    { transaction: t })
-                    .then(ticket => {
-                        ticket['type'] = rps.TicketType.close;
-                        ticket['url'] = url;
-                        ticket['completedAt'] = new Date();
-                        return ticket.save({ transaction: t });
-                    })
-                    .then(() => {
-                        t.commit();
-                    });
+        return this.updateTicket(
+            {
+                id: ticketId,
+                progressUserId: toUserId(user)
+            },
+            {
+                type: rps.TicketType.close,
+                url: url,
+                completedAt: new Date()
             });
     }
 
     reverseToInprogressTicket(user: rps.User, ticketId: string) {
-        return this.transaction()
-            .then(t => {
-                return this.ticket.findOne(
-                    { where: { id: ticketId, progressUserId: toUserId(user) } },
-                    { transaction: t })
-                    .then(ticket => {
-                        ticket['type'] = rps.TicketType.inprogress;
-                        ticket['completedAt'] = null;
-                        return ticket.save({ transaction: t });
-                    })
-                    .then(() => {
-                        t.commit();
-                    });
+        return this.updateTicket(
+            {
+                id: ticketId,
+                progressUserId: toUserId(user)
+            },
+            {
+                type: rps.TicketType.inprogress,
+                completedAt: null
             });
+    }
+
+    private updateTicket(where: any, options: any) {
+        return this.transaction().then(t => {
+            return this.schema.ticket.findOne(
+                { where: where },
+                { transaction: t })
+                .then(ticket => {
+                    for (var key in options) {
+                        ticket[key] = options[key];
+                    }
+                    return ticket.save({ transaction: t });
+                })
+                .then(() => t.commit());
+        });
     }
 
     private transaction() {
---
+++ src/hayo/infrastructure/schema.ts
@@ -0,0 +1,59 @@
+import Sequelize = require('sequelize');
+
+var SERIAL = {
+    type: Sequelize.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    get: function () {
+        return this.getDataValue('id').toString(10);
+    }
+};
+
+class Schema {
+    ticket = this.sequelize.define('Ticket',
+        {
+            id: SERIAL,
+            title: {
+                type: Sequelize.STRING,
+                validate: { len: [1, 32] }
+            },
+            type: Sequelize.INTEGER,
+            url: {
+                type: Sequelize.STRING,
+                validate: { len: [1, 2083] }
+            },
+            openUserId: Sequelize.STRING,
+            progressUserId: Sequelize.STRING,
+            progressAt: Sequelize.DATE,
+            completedAt: Sequelize.DATE
+        }, {
+            updatedAt: false,
+            paranoid: true
+        });
+
+    likeOpen = this.sequelize.define('LikeOpen',
+        {
+            id: SERIAL,
+            userId: Sequelize.STRING
+        }, {
+            timestamps: false
+        });
+
+    likeInprogress = this.sequelize.define('LikeInprogress',
+        {
+            id: SERIAL,
+            userId: Sequelize.STRING,
+            count: Sequelize.INTEGER
+        }, {
+            timestamps: false
+        });
+
+    constructor(private sequelize: Sequelize) {
+        this.ticket.hasMany(this.likeOpen);
+        this.likeOpen.belongsTo(this.ticket);
+        this.ticket.hasMany(this.likeInprogress);
+        this.likeInprogress.belongsTo(this.ticket);
+    }
+}
+
+export = Schema;
